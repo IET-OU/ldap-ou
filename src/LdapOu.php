c8baf2c75a17f496aac9e00caa692ca0b03603cd
Nick Freear <nfreear@yahoo.co.uk>
2019-02-11 17:13:27 +0000

    Break `LdapOu::test()` into separate methods/functions

--- src/LdapOu.php
+++ src/LdapOu.php
@@ -1,8 +1,10 @@
 <?php namespace IET_OU\LdapOu;
 
 /**
+ * Lightweight Directory Access Protocol, for The OU.
  *
  * @copyright  © 2012, 2013, 2019 The Open University (IET).
+ * @author     Nick Freear, 05-Feb-2019.
  */
 
 use Zend\Ldap\Ldap;
@@ -13,7 +15,7 @@ class LdapOu
 {
     const DOT_ENV = __DIR__ . '/..';
     const JSON_FILE = __DIR__ . '/../data/ldap-search.json';
-    const SCHEMA_FILE = __DIR__ . './../data/ldap-schema.txt';
+    const SCHEMA_FILE = __DIR__ . '/../data/ldap-schema.txt';
 
     const HOST = 'DC1.open.ac.uk';
     const PORT = 3268;
@@ -23,6 +25,7 @@ class LdapOu
     const EXAMPLE_OUCU = 'xyzz123';
 
     protected static $io;
+    protected static $ldap;
 
     public static function test(Event $event)
     {
@@ -33,10 +36,37 @@ class LdapOu
             print_r([ \get_class($event), $event->getName(), $event->getArguments(), ]);
         }
 
+        self::loadDotenv();
+
+        self::connect();
+
+        self::dumpSchema();
+
+        $oucuFilter = sprintf(self::FILTER_FORMAT, self::getenvDefault('LDAP_OUCU', self::EXAMPLE_OUCU));
+
+        $result = self::search($oucuFilter);
+
+        file_put_contents( self::JSON_FILE, json_encode( $result->toArray(), JSON_PRETTY_PRINT | JSON_PARTIAL_OUTPUT_ON_ERROR ));
+    }
+
+    protected static function getenvDefault($varname, $default = null)
+    {
+        $result = \getenv($varname) ? \getenv($varname) : $default;
+
+        self::$io->warning("Getenv, $varname: $result", $context = [ ]);
+
+        return $result;
+    }
+
+    protected static function loadDotenv()
+    {
         $dotenv = Dotenv::create(self::DOT_ENV);
         $dotenv->load();
+    }
 
-        $ldap = new \Zend\Ldap\Ldap([
+    protected static function connect()
+    {
+        self::$ldap = new \Zend\Ldap\Ldap([
           'host' => self::getenvDefault('LDAP_OU_HOST', self::HOST),
           'port' => self::getenvDefault('LDAP_OU_PORT', self::PORT),
           'username' => self::getenvDefault('LDAP_OU_USER'), // $ldaprdn,
@@ -46,21 +76,26 @@ class LdapOu
         ]);
 
         if (self::$io->isVerbose()) {
-            self::$io->info(\get_class($ldap));
-            print_r($ldap->getOptions());
+            self::$io->info(\get_class(self::$ldap));
+            print_r(self::$ldap->getOptions());
         }
 
-        $ldap->bind();
+        self::$ldap->bind();
 
+        return self::$ldap;
+    }
+
+    protected static function dumpSchema()
+    {
         if (self::getenvDefault('LDAP_OU_SCHEMA')) {
-            $schema = $ldap->getSchema();
+            $schema = self::$ldap->getSchema();
             file_put_contents( self::SCHEMA_FILE, print_r( $schema, true ));
         }
+    }
 
-        // Staff OUCUs: ...
-        $query = sprintf(self::FILTER_FORMAT, self::getenvDefault('LDAP_OUCU', self::EXAMPLE_OUCU));
-
-        $result = $ldap->search($query, null, Ldap::SEARCH_SCOPE_SUB, [ '*' ] );
+    protected static function search($filter, $scope = Ldap::SEARCH_SCOPE_SUB, $attributes = [ '* '])
+    {
+        $result = self::$ldap->search( $filter, null, $scope, $attributes );
 
         // Empty! $result = $ldap->search('(&(objectClass=student)(sAMAccountName=abc123))', null, Ldap::SEARCH_SCOPE_SUB, [ '*' ] );
 
@@ -72,15 +107,6 @@ class LdapOu
             print_r( $result->toArray() );
         }
         print_r([ $lastLogin , sha1( (string) $lastLogin /* . $oucu . $samaccounttype */ ) ]);
-
-        file_put_contents( self::JSON_FILE, json_encode( $result->toArray(), JSON_PRETTY_PRINT | JSON_PARTIAL_OUTPUT_ON_ERROR ));
-    }
-
-    protected static function getenvDefault($varname, $default = null)
-    {
-        $result = \getenv($varname) ? \getenv($varname) : $default;
-
-        self::$io->warning("Getenv, $varname: $result", $context = [ ]);
 
         return $result;
     }
